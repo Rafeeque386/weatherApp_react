81433b5dbba7c0472a28be2658d775a569a2571a
rafeeque386 <rafeeque386@gmail.com>
2020-10-30 20:21:39 +0530

    style updated

--- src/App.js
+++ src/App.js
@@ -6,6 +6,19 @@ const api={
 
 
 function App() {
+const dateBuilder=(d)=>{
+  let months=["january","February","March","April","May","June","July","August","September","October","November","December"];
+  let days=["sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+  let day=days[d.getDay()];
+  let date=d.getDate();
+  let month=months.[d.getMonth()];
+  let year=d.getFullYear();
+
+
+  return `${day} ${date} ${month} ${year}`
+}
+
   return (
     <div className="app ">
 
@@ -15,6 +28,16 @@ function App() {
         className="search-bar"
         placeholder="search..."/>
       
+    </div>
+    <div className="location-box">
+      <div className="location">New York City,US</div>
+  <div className="date">{dateBuilder(new Date())}</div>
+      </div>
+    <div className="weather-box">
+      <div className="temp">
+        15°c
+      </div>
+      <div className="weather">Sunny</div>
     </div>
     </main>
     </div>

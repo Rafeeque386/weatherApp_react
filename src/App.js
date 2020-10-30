de9c043dbdf7791fd6b7e5f9506794d6225b64ba
rafeeque386 <rafeeque386@gmail.com>
2020-10-30 21:27:10 +0530

    App function added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState} from 'react';
 const api={
   key:"127e678da8d8141fdf2c680e07cb99dd",
   base:"https://api.openweathermap.org/data/2.5/"
@@ -6,6 +6,21 @@ const api={
 
 
 function App() {
+const [query,setQuery]=useState('')
+const [weather,setWeather]=useState({});
+
+const search=evt=>{
+  if(evt.key=="Enter"){
+    fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+    .then(res=>res.json())
+    .then(result=>{
+
+      setWeather(result);
+      setQuery('');
+  });
+}
+}
+
 const dateBuilder=(d)=>{
   let months=["january","February","March","April","May","June","July","August","September","October","November","December"];
   let days=["sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
@@ -20,25 +35,41 @@ const dateBuilder=(d)=>{
 }
 
   return (
-    <div className="app ">
+<div className={
+  (typeof weather.main !="undefined")?((weather.main.temp>16)
+  ? 'app warm'
+  :'app')
+  :'app'}>
+
+
+    
 
       <main>
         <div className="search-box">
         <input type="text"
         className="search-bar"
-        placeholder="search..."/>
+        placeholder="search..."
+        onChange={e=>setQuery(e.target.value)}
+        value={query}
+        onKeyPress={search}
+        
+        />
       
     </div>
+    {(typeof weather.main != "undefined") ?(
+      <div>
     <div className="location-box">
-      <div className="location">New York City,US</div>
+  <div className="location">{weather.name},{weather.sys.country}</div>
   <div className="date">{dateBuilder(new Date())}</div>
       </div>
     <div className="weather-box">
       <div className="temp">
-        15°c
+        {Math.round(weather.main.temp)}°c
       </div>
-      <div className="weather">Sunny</div>
+      <div className="weather">{weather.weather[0].main}</div>
     </div>
+    </div>
+    ):('')}
     </main>
     </div>
   );
